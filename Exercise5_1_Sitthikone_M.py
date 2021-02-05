378b8e113aa3e1078ab602778646d94e075c1cfc
KENGSM <78570868+KENGSM@users.noreply.github.com>
2021-02-06 02:45:29 +0900

    Add files via upload

---
+++ Exercise5_1_Sitthikone_M.py
@@ -0,0 +1,16 @@
+print("-----------------")
+print("Computing 2 unit")
+print("-----------------")
+print("let's start!!!")
+x = int(input("Please insert unit 1 :"))
+y = int(input("Please insert unit 2 :"))
+result = x + y
+result1 = x - y
+result2 = x * y
+result3 = x / y
+print("-----------------")
+print("Result :")
+print(x , "+", y,"=", result)
+print(x , "+", y,"=", result1)
+print(x , "+", y,"=", result2)
+print(x , "+", y,"=", int(result3))
---
+++ Exercise5_2_Sitthikone_M.py
@@ -0,0 +1,11 @@
+print("-----------------")
+print("Welcome to Velocity Calculator")
+print("-----------------")
+vunit = "km/h"
+tunit = "h"
+dunit = "Km"
+d = int(input("Please insert the distance (km) :"))
+t = int(input("Please insert the taken time (h) :"))
+v = (d / t)
+print("-----------------")
+print (int(v), vunit)
