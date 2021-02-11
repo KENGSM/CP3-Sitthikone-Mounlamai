8e81570c86f79e5ebf6380bba140124c466fd0da
KENGSM <78570868+KENGSM@users.noreply.github.com>
2021-02-11 11:59:37 +0900

    Add files via upload

---
+++ Lecture 50 funtion.py
@@ -0,0 +1,19 @@
+def addNumber(x,y):
+    print(x+y)
+
+addNumber(100,5)
+
+def minusNumber(x,y):
+    print(x-y)
+
+minusNumber(100, 5)
+
+def mutiplyNumber(x,y):
+    print(x*y)
+
+mutiplyNumber(100,5)
+
+def divideNumber(x,y):
+    print(int(x/y))
+
+divideNumber(100,5)
